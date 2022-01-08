941820e0a8be1e882183a76e0539afa1fc958b15
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2022-01-09 00:31:39 +0200

    Add book constructor

---
+++ app.js
@@ -0,0 +1,8 @@
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+  this.info = () =>
+    `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead}`;
+}
