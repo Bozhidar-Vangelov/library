f02feaa2a2707e74378ac42da71922c3b949b935
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2022-01-11 12:16:50 +0200

    Add toggle logic

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const addNewBook = document.getElementById('add-new');
+const modal = document.getElementById('modal');
+const form = document.getElementById('form');
+const overlay = document.getElementById('overlay');
+const addBook = document.getElementById('add-book');
+
 function Book(title, author, pages, isRead) {
   this.title = title;
   this.author = author;
@@ -6,3 +12,6 @@ function Book(title, author, pages, isRead) {
   this.info = () =>
     `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead}`;
 }
+
+addNewBook.addEventListener('click', () => (modal.style.display = 'block'));
+overlay.addEventListener('click', () => (modal.style.display = 'none'));
